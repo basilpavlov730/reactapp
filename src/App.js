e3c22d1fa031622aa3ed71e6af568b6c1823d8ee
Vasiliy Pavlov <vasiliy.pavlov@solisoft.group>
2023-06-03 13:37:36 +0300

    Add interservice interaction

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import React from 'react';
 import Header from './components/header'
 import UserList from './components/UserList';
 import AddUser from './components/addUser';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  baseUrl = 'http://localhost:5000/api/Users'
+
     constructor(props){
         super(props)
 
         this.state = {
             counter: 0,
             userText: "",
-            users: [{
-              id: 1,
-              name: "Pasha",
-              email: "pasha@stankin.ru"
-          },{
-              id: 2,
-              name: "Sasha",
-              email: "sasha@stankin.ru"
-          },{
-              id: 3,
-              name: "Masha",
-              email: "masha@stankin.ru"
-          }]
+            users: []
         }
+
+        this.LoadUsers();
 
         this.AddUser = this.AddUser.bind(this)
         this.EditUser = this.EditUser.bind(this)
@@ -45,24 +39,65 @@ class App extends React.Component {
       )
     }
 
-    AddUser(user){
-      const userId = this.state.users.length + 1
-      this.setState({users: [...this.state.users, {userId, ...user}]})
-    }
+    LoadUsers(){
+      axios.get(this.baseUrl).then(res => {
+          var userDtos = [];
+          res.data.forEach(element => {
+            userDtos.push({
+              id: element.id,
+              name: element.name,
+              email: element.email
+            })
+          });
 
-    EditUser(user){
-      var allUsers = this.state.users
-      var userToEditIndex = allUsers.findIndex(x => x.id === user.id)
-
-      allUsers[userToEditIndex] = user
-      
-      this.setState({users: []}, () => this.setState({users: [...allUsers]}))
-    }
-
-    DeleteUser(id){
-      this.setState({
-        users: this.state.users.filter(x => x.id !== id)
+          this.setState({
+            users: []
+          }, () => this.setState({
+            users: userDtos
+          }))
       })
+    }
+
+    async AddUser(user){
+
+      var createUserRequest = {
+        UserName: user.name,
+        Email: user.email
+      }
+
+      await axios.post(this.baseUrl, createUserRequest)
+
+      this.LoadUsers();
+      // const userId = this.state.users.length + 1
+      // this.setState({users: [...this.state.users, {userId, ...user}]})
+    }
+
+    async EditUser(user){
+
+      var updateUserRequest = {
+        UserName: user.name,
+        Email: user.email
+      }
+
+      await axios.put(this.baseUrl + '/' + user.id, updateUserRequest)
+
+      this.LoadUsers()
+
+      // var allUsers = this.state.users
+      // var userToEditIndex = allUsers.findIndex(x => x.id === user.id)
+
+      // allUsers[userToEditIndex] = user
+      
+      // this.setState({users: []}, () => this.setState({users: [...allUsers]}))
+    }
+
+    async DeleteUser(id){
+      await axios.delete(this.baseUrl + '/' + id)
+
+      this.LoadUsers()
+      // this.setState({
+      //   users: this.state.users.filter(x => x.id !== id)
+      // })
     }
 
     onInputClick(){
